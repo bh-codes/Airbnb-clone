bc4b1e3d4a5e506aad1b1cf9c41782b245a09f04
bet_h <bettybhan@gmail.com>
2022-11-29 10:17:47 -0500

    Add data file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 
 function App() {
+  const lessons = data.map(item => {
+    return <Card img={item.coverImg} location={item.location} rating={item.stats.rating} reviewCount={item.stats.reviewCount} title={item.title} price={item.price}/>
+  })
   return (
     <body>
       <Navbar />
       <Hero />
-      <Card />
+      {lessons}
     </body>
   )
 }
