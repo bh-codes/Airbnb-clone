ed96aa0900bb030ee4f75f9687ec367ca6a0003b
bet_h <bettybhan@gmail.com>
2022-11-29 10:18:40 -0500

    Update card component and add images

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
 import React from "react"
 
-function Card() {
+function Card(props) {
     return (
         <div className="card">
-            <img className="katie" src={require("../images/katie-zaferes.png")} alt="Image of Katie Zaferes"/>
+            <img className="katie" src={require(`../images/${props.img}`)} alt="Image of Katie Zaferes"/>
             <div className="card-stat">
-                <p className="rating"><img className="star-icon" src={require("../images/star.png")} width="14px" alt="star rating" />5.0 <span className="star-rating">(6) • USA</span></p>
-                <p className="katie-title">Life lessons with Katie Zaferes</p>
-                <p className="rate-price"><span className="price">From $136</span> / person</p>
+                <p className="rating"><img className="star-icon" src={require("../images/star.png")} width="14px" alt="star rating" />{props.rating} <span className="star-rating">({props.reviewCount}) • {props.location}</span></p>
+                <p className="katie-title">{props.title}</p>
+                <p className="rate-price"><span className="price">From ${props.price}</span> / person</p>
                 </div>
             </div>
     )
